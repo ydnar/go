292de70cb798b4b18b7a30fa0c0b7e75ece3e19d
Randy Reddig <randy@alta.software>
2021-10-06 08:29:51 -0700

    encoding/xml: add test to ensure '@' is legal in an attr val

--- src/encoding/xml/marshal_test.go
+++ src/encoding/xml/marshal_test.go
@@ -1079,6 +1079,10 @@ var marshalTests = []struct {
 		Value:     &OmitAttrTest{},
 		ExpectXML: `<OmitAttrTest></OmitAttrTest>`,
 	},
+	{
+		Value:     &OmitAttrTest{Str: "gopher@golang.org"},
+		ExpectXML: `<OmitAttrTest Str="gopher@golang.org"></OmitAttrTest>`,
+	},
 
 	// pointer fields
 	{
